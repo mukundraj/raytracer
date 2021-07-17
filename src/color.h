fa80282ec26b2de111997ede7d98c8b1e39422f7
mukundraj <mukundraj2@gmail.com>
2021-07-16 22:07:45 -0400

    Gamma correction and fix shadow acne

--- src/color.h
+++ src/color.h
@@ -13,9 +13,9 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
 
 
     auto scale = 1.0/samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = (sqrt(scale * r));
+    g = (sqrt(scale * g));
+    b = (sqrt(scale * b));
 
 
     // Write the translated [0,255] value of each color component.
